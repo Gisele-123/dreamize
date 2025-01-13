5bb3fb4eddd05ec2ae04b676259f20e6b78045d4
Gisele-123 <giselemigisha53@gmail.com>
2025-01-13 08:31:22 -0800

    week2: day1 added

---
+++ week2/c-files/arrays.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	int numbers[]={10,20,30,40,50};
+	printf("Element 1: %d\n", numbers[0]);
+	printf("Element 2: %d\n", numbers[1]);
+	printf("Element 3: %d\n", numbers[2]);
+	printf("Element 4: %d\n", numbers[3]);
+	printf("Element 5: %d\n", numbers[4]);
+	return 0;
+}
